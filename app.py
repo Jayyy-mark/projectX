991132568ebdca3d5ec7958c06260e4d45de1a33
Jayyy-mark <jaycoab2@gmail.com>
2025-08-16 19:59:01 +0630

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def generate_chart(text,filename="chart.png"):
 
 #create LLM
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     temperature=0,
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
